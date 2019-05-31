31ebbf7f864abb76b5df8dc0777095b10fc979f8
Roman Xz <technopriest666@gmail.com>
2019-05-31 22:21:33 +0300

    Updates

---
+++ precache-manifest.ca844c9f794e82b45ddc0167ddbe44df.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edb0107e400c973a438445cdb67493c",
+    "url": "/coding-profile-page/index.html"
+  },
+  {
+    "revision": "d71b17a06a162de3e031",
+    "url": "/coding-profile-page/static/css/2.f83c72cd.chunk.css"
+  },
+  {
+    "revision": "f00ba27dc61b3d816f47",
+    "url": "/coding-profile-page/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d71b17a06a162de3e031",
+    "url": "/coding-profile-page/static/js/2.fe54909e.chunk.js"
+  },
+  {
+    "revision": "f00ba27dc61b3d816f47",
+    "url": "/coding-profile-page/static/js/main.c7a00c36.chunk.js"
+  },
+  {
+    "revision": "0fc73e4daf2ff4d79ab7",
+    "url": "/coding-profile-page/static/js/runtime~main.b9c5d62e.js"
+  },
+  {
+    "revision": "117a3d1ebc6cfff415368f09bdf87055",
+    "url": "/coding-profile-page/static/media/orbitron-latin-400.117a3d1e.woff2"
+  },
+  {
+    "revision": "967e2419ec783a764a1875258ce3e3f7",
+    "url": "/coding-profile-page/static/media/orbitron-latin-400.967e2419.woff"
+  },
+  {
+    "revision": "3b539ae483c963b9c76f4f4a3378860f",
+    "url": "/coding-profile-page/static/media/orbitron-latin-500.3b539ae4.woff"
+  },
+  {
+    "revision": "5427b0428d7de2f816c41c40352f5999",
+    "url": "/coding-profile-page/static/media/orbitron-latin-500.5427b042.woff2"
+  },
+  {
+    "revision": "aabeba094bd629c16469769a868db99e",
+    "url": "/coding-profile-page/static/media/orbitron-latin-700.aabeba09.woff"
+  },
+  {
+    "revision": "c675b19e05021dc5810e1c51282207ce",
+    "url": "/coding-profile-page/static/media/orbitron-latin-700.c675b19e.woff2"
+  },
+  {
+    "revision": "a3d485e65fd80ca755a6ae00fe112d48",
+    "url": "/coding-profile-page/static/media/orbitron-latin-900.a3d485e6.woff2"
+  },
+  {
+    "revision": "cdec72ff64e7848e05a569edf06ebac8",
+    "url": "/coding-profile-page/static/media/orbitron-latin-900.cdec72ff.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b1e01049971cf505edd1ed7059aeb8c.js"
+  "/coding-profile-page/precache-manifest.ca844c9f794e82b45ddc0167ddbe44df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coding-profile-page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
